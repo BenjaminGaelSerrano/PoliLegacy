0643ebfb27b663df2ec2e566e1da054d09777168
lulorojas <lulorojas11@gmail.com>
2026-06-28 23:12:06 -0300

    subo jugador y su proyectil

--- poly-legacy/Scripts/busEventos.gd
+++ poly-legacy/Scripts/busEventos.gd
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 extends Node2D
 signal jugadorSeMueve
 signal jugadorHablaConNpc
@@ -10,3 +11,10 @@ signal jefeDerrotado
 signal jugadorRecibioDanio
 signal jugadorMuerto
 signal coleccionableObtenido
+=======
+extends Node
+
+signal boss_derrotado(numero_nivel)
+signal jugador_recibio_danio(vida_actual)
+signal jugador_murio
+>>>>>>> db0d56c (subo jugador y su proyectil)
