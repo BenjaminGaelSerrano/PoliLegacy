a74f20d48c53f5c0c7e5f5e8ef01258f34932f0c
Benjamín-Serrano <alumno26.serrano.benjamin@ipm.edu.ar>
2026-06-30 17:50:10 -0300

    Algunos ajustes

---
+++ poly-legacy/Scripts/Inventario.gd
@@ -0,0 +1,13 @@
+extends Node2D
+var items=[]
+func agregarItem(esp: String, tipo: String, datos: Dictionary):
+	items.append({
+		"EspacioEnElInventario": esp,
+		"tipo": tipo,
+		"datos": datos
+	})
+func tieneItem(esp: String) -> bool:
+	for item in items:
+		if item.EspacioEnElInventario == esp:
+			return true
+	return false
