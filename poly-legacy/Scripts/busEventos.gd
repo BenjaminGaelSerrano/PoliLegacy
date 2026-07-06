096d4f2ecf5676d84d1e7c8c782d40ea0c190533
lulorojas <lulorojas11@gmail.com>
2026-07-06 09:20:40 -0300

    subo para ergerar

--- poly-legacy/Scripts/busEventos.gd
+++ poly-legacy/Scripts/busEventos.gd
@@ -1,20 +1,13 @@
 <<<<<<< HEAD
 extends Node2D
-signal jugadorSeMueve
+signal jugadorSeMovio
 signal jugadorHablaConNpc
 signal libroEntregado
 signal libroAgarrado
 signal libroLeido
-signal disparo
+signal disparoRealizado
 signal tutorialCompletado
-signal jefeDerrotado(nivel)
-signal jugadorRecibioDanio(vida)
-signal jugadorMuerto
+signal jefeDerrotado
+signal jugadorRecibioDanio
+signal jugadorMurio
 signal coleccionableObtenido
-=======
-extends Node
-
-signal boss_derrotado(numero_nivel)
-signal jugador_recibio_danio(vida_actual)
-signal jugador_murio
->>>>>>> db0d56c (subo jugador y su proyectil)
